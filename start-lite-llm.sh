e25450ae51eb7a9c1a6de4da750dd4aa3290c49d
TylerCollison <tylerc2089@hotmail.com>
2026-07-01 00:14:52 +0000

    Running lite llm in the background

--- start-lite-llm.sh
+++ start-lite-llm.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/with-contenv bash
 
-litellm --host 127.0.0.1 --port 5090 --config /lite-llm/lite-llm-default.yaml
+litellm --host 127.0.0.1 --port 5090 --config /lite-llm/lite-llm-default.yaml & 
